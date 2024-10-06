569e2fb335e8b4a7c822e46dea9aadeb50d69962
Adrian Złotkowski <adrianzlotkowski011@gmail.com>
2024-10-06 19:27:01 +0200

    initial commit

---
+++ src/main/java/com/demo/montecarlopinumber/MonteCarloPiNumberApplication.java
@@ -0,0 +1,13 @@
+package com.demo.montecarlopinumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonteCarloPiNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonteCarloPiNumberApplication.class, args);
+	}
+
+}
